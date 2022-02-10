2aba8da7e54d2674cb8a38f376c3c183e16c43a0
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-10 17:11:10 -0600

    Created `FeedItem` model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Dillon on 2/10/22.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
