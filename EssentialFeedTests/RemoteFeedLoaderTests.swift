88d4685a32689d8203e9ed1919c016ad1db22fe0
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-10 17:29:03 -0600

    Replaces default `EssentialFeedTests` - Adds new `XCTestCase` subclass `RemoteFeedLoaderTests` - Adds test to ensure no data is loaded on init of `RemoteFeedLoader` module

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Dillon on 2/10/22.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}
