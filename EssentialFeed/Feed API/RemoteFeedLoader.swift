41c873f70b7b861c3511e7a4c903ff473886fcc4
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-11 13:12:28 -0600

    Refactor `HTTPClientSpy` capturing behavior - Combines completions and URLs into single array `messages` - Captures `get(url:completion:)` invocations in `messages` array

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-/// The  `HTTPClient` protocol enforces functionality to be implemented
+/// The `HTTPClient` protocol enforces functionality to be implemented
 /// by any of the EssentialFeed application API modules. The public protocol
 /// can be implemented by external modules to initiate a networking request.
 public protocol HTTPClient {
