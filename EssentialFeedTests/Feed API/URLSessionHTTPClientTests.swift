25cb2c5f9db163be9e93354b8383f3f3c12ff4a2
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-14 16:47:48 -0600

    Move URL creation to shared factory method

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -43,7 +43,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     /// This test allows us to determine if there is an issue with the url itself, or if there
     /// is an issue somewhere else (such as with the request or SUT).
     func test_getFromURL_performsGETRequestWithURL() {
-        let url = URL(string: "https://any-url.com")!
+        let url = anyURL()
         let exp = expectation(description: "Wait for request")
         
         URLProtocolStub.observeRequests { request in
@@ -58,7 +58,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsWhenURLDoesntMatchObservedRequestURL() {
-        let url = URL(string: "https://a-url.com")!
+        let url = anyURL()
         let exp = expectation(description: "Wait for request")
         
         URLProtocolStub.observeRequests { request in
@@ -74,13 +74,12 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "any error", code: 1, userInfo: nil)
         URLProtocolStub.stub(data: nil, response: nil,  error: error)
         
         let exp = expectation(description: "Wait for completion")
         
-        makeSUT().get(from: url) { result in
+        makeSUT().get(from: anyURL()) { result in
             switch result {
                 case .failure(let receivedError as NSError):
                     XCTAssertEqual(error, receivedError.omittingUserInfo)
@@ -104,6 +103,10 @@ extension URLSessionHTTPClientTests {
         let sut = URLSessionHTTPClient()
         assertNoMemoryLeaks(sut, objectName: "SUT", file: file, line: line)
         return sut
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
 }
 
