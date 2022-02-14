eadb4ea1521250844f162ae9077a9c875c746f0c
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-13 21:38:32 -0600

    Test delivers failure when there's a request error

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -16,12 +16,54 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { _, _, _ in }.resume()
+        session.dataTask(with: url) { _, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
     
+    func test_getFromURL_failsOnRequestError() {
+        URLProtocolStub.startInterceptingRequests()
+        let url = URL(string: "https://any-url")!
+        let error = NSError(domain: "any error", code: 1, userInfo: nil)
+        URLProtocolStub.stub(url: url, error: error)
+        
+        let sut = URLSessionHTTPClient()
+        
+        let exp = expectation(description: "Wait for completion")
+        
+        sut.get(from: url) { result in
+            switch result {
+                case .failure(let receivedError as NSError):
+                    XCTAssertEqual(error, receivedError.omittingUserInfo)
+                default:
+                    XCTFail("Expected failure with error \(error), got \(result) instead.")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopInterceptingRequests()
+    }
+}
+
+
+/// iOS 14+ replaces received errors via `URLSession` with a new
+/// error instance. The new instance includes `userInfo` data, which
+/// cause our tests to fail when comparing sent/received errors for
+/// equality. Use the `omittingUserInfo` property to return an error
+/// with the original `domain` and `code` to use `XCTAssertEqual`
+/// to compare the sent and received errors.
+private extension NSError {
+    var omittingUserInfo: NSError {
+        /// Return an NSError copy using properties of self w/o userInfo
+        return NSError(domain: self.domain, code: self.code, userInfo: nil)
+    }
 }
 
 
@@ -36,6 +78,15 @@ extension URLSessionHTTPClientTests {
         
         static func stub(url: URL, error: Error?) {
             stubs[url] = Stub(error: error)
+        }
+        
+        static func startInterceptingRequests() {
+            URLProtocol.registerClass(URLProtocolStub.self)
+        }
+        
+        static func stopInterceptingRequests() {
+            URLProtocol.unregisterClass(URLProtocolStub.self)
+            stubs = [:]
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
