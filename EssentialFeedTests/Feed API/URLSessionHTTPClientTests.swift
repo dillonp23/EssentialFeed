85e0e381590aff07b269fb5998aea2f8158e99a7
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-14 11:28:09 -0600

    Improves `URLProtocolStub` - Adds `Data?` and `URLResponse?` properties to `Stub` type - Enables more explicit setup & test cases in future iterations

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -24,13 +24,14 @@ class URLSessionHTTPClient {
     }
 }
 
+
 class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_failsOnRequestError() {
         URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "https://any-url")!
         let error = NSError(domain: "any error", code: 1, userInfo: nil)
-        URLProtocolStub.stub(url: url, error: error)
+        URLProtocolStub.stub(url: url, data: nil, response: nil,  error: error)
         
         let sut = URLSessionHTTPClient()
         
@@ -53,33 +54,23 @@ class URLSessionHTTPClientTests: XCTestCase {
 }
 
 
-/// iOS 14+ replaces received errors via `URLSession` with a new
-/// error instance. The new instance includes `userInfo` data, which
-/// cause our tests to fail when comparing sent/received errors for
-/// equality. Use the `omittingUserInfo` property to return an error
-/// with the original `domain` and `code` to use `XCTAssertEqual`
-/// to compare the sent and received errors.
-private extension NSError {
-    var omittingUserInfo: NSError {
-        /// Return an NSError copy using properties of self w/o userInfo
-        return NSError(domain: self.domain, code: self.code, userInfo: nil)
-    }
-}
-
-
+// MARK: - URLProtocol Stubbing
 extension URLSessionHTTPClientTests {
     
     private class URLProtocolStub: URLProtocol {
         private static var stubs = [URL: Stub]()
         
         private struct Stub {
+            let data: Data?
+            let response: URLResponse?
             let error: Error?
         }
         
-        static func stub(url: URL, error: Error?) {
-            stubs[url] = Stub(error: error)
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
+            stubs[url] = Stub(data: data, response: response, error: error)
         }
         
+        // MARK: Registering & Unregistering URLProtocol
         static func startInterceptingRequests() {
             URLProtocol.registerClass(URLProtocolStub.self)
         }
@@ -89,19 +80,29 @@ extension URLSessionHTTPClientTests {
             stubs = [:]
         }
         
+        // MARK: URLProtocol Pre-Initialization Setup
         override class func canInit(with request: URLRequest) -> Bool {
             guard let url = request.url else { return false }
             
-            return stubs[url] != nil
+            return URLProtocolStub.stubs[url] != nil
         }
         
         override class func canonicalRequest(for request: URLRequest) -> URLRequest {
             return request
         }
         
+        // MARK: Start & Stop Loading URLProtocool
         override func startLoading() {
             guard let url = request.url,
-                    let stub = URLProtocolStub.stubs[url] else { return }
+                  let stub = URLProtocolStub.stubs[url] else { return }
+            
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
             
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
@@ -111,5 +112,22 @@ extension URLSessionHTTPClientTests {
         }
         
         override func stopLoading() {}
+    }
+}
+
+
+// MARK: - Helpers
+/// This private extension allows us to format errors received back from a `URLSession` to
+/// an `NSError` instance with properties matching the orignally passed in error, to enable
+/// us to compare the two errors for equality in our test case assertions.
+///
+/// iOS 14+ replaces received errors via `URLSession` with a new error instance. This new
+/// instance includes `userInfo` data, which causes our tests to fail when comparing sent &
+/// received errors for equality. Use the `omittingUserInfo` computed property to return
+/// an error with the original `domain` and `code` to enable `XCTAssertEqual` usage.
+private extension NSError {
+    var omittingUserInfo: NSError {
+        /// Return an NSError copy using properties of self w/o userInfo
+        return NSError(domain: self.domain, code: self.code, userInfo: nil)
     }
 }
