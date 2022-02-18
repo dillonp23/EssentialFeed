545e8684f8beb4d60ccfacb3359af7498beab9d5
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-18 17:20:51 -0600

    Tests cache not deleted on `LocalFeedLoader` init

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -8,6 +8,22 @@
 import Foundation
 import XCTest
 
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
 class CacheFeedUseCaseTests: XCTestCase {
     
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
 }
