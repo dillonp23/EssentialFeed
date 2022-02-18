a9e8014e02e7a1f89014a6e2d78ef314870afe05
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-18 17:13:50 -0600

    Adds a `XCTestCase` for caching - Moves `Helpers` group to a shared location in test target

---
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,13 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Dillon on 2/18/22.
+//
+
+import Foundation
+import XCTest
+
+class CacheFeedUseCaseTests: XCTestCase {
+    
+}
