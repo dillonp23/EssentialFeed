fe50eb77c58d5ee2af640055f4ada15773ea9a76
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-22 17:11:20 -0600

    Adds `ValidateCachedFeedUseCaseTests` - New class to enable extraction of cache validation from loading logic

---
+++ EssentialFeedTests/Feed Cache/ValidateCachedFeedUseCaseTests.swift
@@ -0,0 +1,14 @@
+//
+//  ValidateCachedFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Dillon on 2/22/22.
+//
+
+import Foundation
+import XCTest
+import EssentialFeed
+
+class ValidateCachedFeedUseCaseTests: XCTestCase {
+    
+}
