6909385a6c9b801446bc684c61b0a9d3dd1be47c
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-22 17:21:42 -0600

    Ensures no invocations on store w/ SUT init - `LocalFeedLoader` doesn't message store upon creation (before validating cached feed)

--- EssentialFeedTests/Feed Cache/ValidateCachedFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/ValidateCachedFeedUseCaseTests.swift
@@ -11,4 +11,22 @@ import EssentialFeed
 
 class ValidateCachedFeedUseCaseTests: XCTestCase {
     
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    
+    // MARK: - Helpers
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        assertNoMemoryLeaks(store, objectName: "FeedStore_Cache", file: file, line: line)
+        assertNoMemoryLeaks(sut, objectName: "LocalFeedLoader_Cache", file: file, line: line)
+        
+        return (sut, store)
+    }
 }
