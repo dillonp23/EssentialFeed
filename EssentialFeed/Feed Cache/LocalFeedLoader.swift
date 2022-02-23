9a810df66af9526e75683fcc5ef616c0ca01d45e
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-23 16:19:54 -0600

    Extract `FeedCachePolicy` to its own file

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -7,24 +7,6 @@
 
 import Foundation
 
-enum FeedCachePolicy {
-    enum Status {
-        case expired
-        case notExpired
-    }
-    
-    private static let calendar = Calendar(identifier: .gregorian)
-    private static let maxCacheAgeInDays = 7
-    
-    static func validateExpirationStatus(for timestamp: Date, against currentDate: Date) -> Status {
-        guard let expiration = calendar.date(byAdding: .day, value: -maxCacheAgeInDays, to: currentDate) else {
-            preconditionFailure("`CacheValidationPolicy` error: Unable to get expiration timestamp from \(currentDate)")
-        }
-        
-        return timestamp <= expiration ? .expired : .notExpired
-    }
-}
-
 public class LocalFeedLoader: FeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
