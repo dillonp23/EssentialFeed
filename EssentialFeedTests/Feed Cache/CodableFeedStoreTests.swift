a21b8fe8c227da951dc47ab11f3624881c4b9a07
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-24 15:15:46 -0600

    SUT's `retrieve()` delivers empty on empty cache

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -6,8 +6,31 @@
 //
 
 import Foundation
+import EssentialFeed
 import XCTest
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+}
 
 class CodableFeedStoreTests: XCTestCase {
     
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "Wait for retrieval completion")
+        
+        sut.retrieve { result in
+            switch result {
+                case .empty:
+                    break
+                default:
+                    XCTFail("Expected empty result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
 }
