59face54108de1896aa5701ed36426941da30be3
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-24 15:09:01 -0600

    Adds `CodableFeedStoreTests`

---
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,13 @@
+//
+//  CodableFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Dillon on 2/24/22.
+//
+
+import Foundation
+import XCTest
+
+class CodableFeedStoreTests: XCTestCase {
+    
+}
