a0f51bf5423b8eac8e905632e714cbc6e0219613
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-02-25 19:51:57 -0600

    Retrieving twice w/ error delivers same failure - Ensures no side effects & that result is reproducible

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -119,6 +119,15 @@ class CodableFeedStoreTests: XCTestCase {
         
         expect(sut, toCompleteRetrievalWith: .failure(anyNSError()))
     }
+    
+    func test_retrieve_hasNoSideEffectsOnRetrievalError() {
+        let storeURL = testSpecificStoreURL
+        let sut = makeSUT(storeURL: storeURL)
+        
+        try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
+        
+        expect(sut, toCompleteRetrievalTwiceWith: .failure(anyNSError()))
+    }
 }
 
 
