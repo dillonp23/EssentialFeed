18f42ea84dec24243be2af29a4d6afdad34deec9
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-03-03 16:05:38 -0600

    Adds `CoreDataStackTests` class

---
+++ EssentialFeedTests/Feed Cache/Core Data/FeedStore+CoreDataStackTests.swift
@@ -0,0 +1,14 @@
+//
+//  FeedStore+CoreDataStackTests.swift
+//  EssentialFeedTests
+//
+//  Created by Dillon on 3/3/22.
+//
+
+import XCTest
+import EssentialFeed
+import CoreData
+
+class CoreDataStackTests: XCTestCase {
+    
+}
