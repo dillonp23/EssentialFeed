e2c4a9b278ca8eb70a9bb5be7359acf425f842d3
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-03-02 20:36:27 -0600

    Adds new file for testing CoreDataStack setup & loading

---
+++ EssentialFeedTests/Feed Cache/Core Data/FeedStore+CoreDataStackTests.swift
@@ -0,0 +1,14 @@
+//
+//  FeedStore+CoreDataStackTests.swift
+//  EssentialFeedTests
+//
+//  Created by Dillon on 3/2/22.
+//
+
+import XCTest
+import EssentialFeed
+import CoreData
+
+extension CoreDataFeedStoreTests {
+    
+}
