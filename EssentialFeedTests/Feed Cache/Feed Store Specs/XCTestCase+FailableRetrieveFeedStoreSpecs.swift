444bccae1119e8c73de9cae3b871e005273a01a5
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-03-04 17:19:48 -0600

    Deletes unnecessary `CoreDataFeedStore` ext. & enables helper reuse - Updates `storeURL` param as optional w/ default of nil (to ignore with core data implementation)

--- EssentialFeedTests/Feed Cache/Feed Store Specs/XCTestCase+FailableRetrieveFeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/Feed Store Specs/XCTestCase+FailableRetrieveFeedStoreSpecs.swift
@@ -9,32 +9,23 @@ import Foundation
 import XCTest
 import EssentialFeed
 
-extension FailableRetrieveFeedStoreSpecs where Self: CodableFeedStoreTests {
-    func assertRetrieveDeliversFailureOnFailedRetrieval(usingStore sut: FeedStore, storeURL: URL,
+extension FailableRetrieveFeedStoreSpecs where Self: XCTestCase {
+    func assertRetrieveDeliversFailureOnFailedRetrieval(usingStore sut: FeedStore, storeURL: URL? = nil,
                                                         file: StaticString = #filePath, line: UInt = #line) {
         writeInvalidDataForRetrieval(to: storeURL)
         expect(sut, toCompleteRetrievalWith: .failure(anyNSError()), file: file, line: line)
     }
     
-    func assertRetrieveHasNoSideEffectsOnFailedRetrieval(usingStore sut: FeedStore, storeURL: URL,
+    func assertRetrieveHasNoSideEffectsOnFailedRetrieval(usingStore sut: FeedStore, storeURL: URL? = nil,
                                                          file: StaticString = #filePath, line: UInt = #line) {
         writeInvalidDataForRetrieval(to: storeURL)
         expect(sut, toCompleteRetrievalTwiceWith: .failure(anyNSError()), file: file, line: line)
     }
     
-    private func writeInvalidDataForRetrieval(to storeURL: URL) {
+    /// `CodableFeedStore` requires non-empty invalid data to cause failure (ignored for CoreData store)
+    private func writeInvalidDataForRetrieval(to storeURL: URL?) {
+        guard let storeURL = storeURL else { return }
+        
         try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
-    }
-}
-
-extension FailableRetrieveFeedStoreSpecs where Self: CoreDataFeedStoreTests {
-    func assertRetrieveDeliversFailureOnFailedRetrieval(usingStore sut: FeedStore,
-                                                        file: StaticString = #filePath, line: UInt = #line) {
-        expect(sut, toCompleteRetrievalWith: .failure(anyNSError()), file: file, line: line)
-    }
-    
-    func assertRetrieveHasNoSideEffectsOnFailedRetrieval(usingStore sut: FeedStore,
-                                                         file: StaticString = #filePath, line: UInt = #line) {
-        expect(sut, toCompleteRetrievalTwiceWith: .failure(anyNSError()), file: file, line: line)
     }
 }
