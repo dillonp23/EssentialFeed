d70a49ddb7e4bbefad0b599e58e6dbfbc743f126
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-03-03 18:19:36 -0600

    Adds `CoreDataFeedStoreTests` extension for `FailableFeedStoreSpecs` - Populates protocol function requirements (empty skeleton)

---
+++ EssentialFeedTests/Feed Cache/Core Data/CoreDataFeedStore+FailableSpecsTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataFeedStore+FailableSpecsTests.swift
+//  EssentialFeedTests
+//
+//  Created by Dillon on 3/3/22.
+//
+
+import Foundation
+import XCTest
+import EssentialFeed
+
+extension CoreDataFeedStoreTests: FailableFeedStoreSpecs {
+    func test_retrieve_deliversFailureOnFailedRetrieval() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnFailedRetrieval() {
+        
+    }
+    
+    func test_insert_deliversErrorOnFailedInsertion() {
+        
+    }
+    
+    func test_insert_hasNoSideEffectsOnFailedInsertion() {
+        
+    }
+    
+    func test_delete_deliversErrorOnFailedDeletion() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnFailedDeletion() {
+        
+    }
+}
