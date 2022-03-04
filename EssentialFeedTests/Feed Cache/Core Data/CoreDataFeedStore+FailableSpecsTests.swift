b97e85c91e086e1d399bae7639ad8e6051674a4d
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-03-03 20:10:44 -0600

    Adds custom `FailableCoreDataStore` class for failable specs tests - Ensures successful load of persistent stores in `CoreDataFeedStore` container using the failable store test-specific class

--- EssentialFeedTests/Feed Cache/Core Data/CoreDataFeedStore+FailableSpecsTests.swift
+++ EssentialFeedTests/Feed Cache/Core Data/CoreDataFeedStore+FailableSpecsTests.swift
@@ -10,6 +10,10 @@ import XCTest
 import EssentialFeed
 
 extension CoreDataFeedStoreTests: FailableFeedStoreSpecs {
+    func test_init_isAbleToLoadPersistentStoresForFailableStore() {
+        _ = makeSUT()
+    }
+    
     func test_retrieve_deliversFailureOnFailedRetrieval() {
         
     }
@@ -32,5 +36,51 @@ extension CoreDataFeedStoreTests: FailableFeedStoreSpecs {
     
     func test_delete_hasNoSideEffectsOnFailedDeletion() {
         
+    }
+}
+
+
+// MARK: Helpers
+extension CoreDataFeedStoreTests {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let sut = try! CoreDataFeedStore(storeType: .custom(store: FailableCoreDataStore.self), bundle: storeBundle)
+        assertNoMemoryLeaks(sut, objectName: "`CoreDataFeedStore`", file: file, line: line)
+        return sut
+    }
+}
+
+// MARK: Helper Class to Test Failable Specs
+private final class FailableCoreDataStore: NSIncrementalStore {
+    public override func loadMetadata() throws {
+        self.metadata = [NSStoreTypeKey: Self.storeTypeKey, NSStoreUUIDKey: Self.storeUUIDKey]
+    }
+}
+
+extension FailableCoreDataStore: CustomCoreDataStore {
+    /// Note: the metdata associated with this type is based on its location in target,
+    /// as well as its access level. Moving the class and/or changing it's access level
+    /// will change the metadata type key, thus requiring a change to this value in code.
+    ///
+    /// Because this class is private (to prevent any access outside of this test class),
+    /// the metadata is an obscure token (see below) and we cannot use the standard human
+    /// readable type key of: "EssentialFeedTests.FailableCoreDataStore"
+    ///
+    /// If you receive an error reading "The store type in the metadata does not match
+    /// the specified store type", copy the token from the error for the 'NSStoreType'
+    /// key and update the value below to ensure class registeration and store loading
+    /// succeeds as expected.
+    public static var storeTypeKey: String {
+        /// *Uncomment next line to see metadata error po in console*
+        // "EssentialFeedTests.FailableCoreDataStore"
+        "_TtC18EssentialFeedTestsP33_6F144DBF9C595C744714877AB9E6EB7921FailableCoreDataStore"
+    }
+    
+    public static var storeUUIDKey: String {
+        "CoreDataFeedStore+FailableSpecsTests"
+    }
+    
+    public static func registerType() {
+        NSPersistentStoreCoordinator.registerStoreClass(FailableCoreDataStore.self, type: .init(rawValue: Self.storeTypeKey))
     }
 }
