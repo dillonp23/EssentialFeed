76833d1a94a0ff83f6dae6b2357e53295eca8e0d
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-03-05 15:36:07 -0600

    Adds `EssentialFeedCacheIntegrationTests` target to separate tests - Keep faster isolated/unit tests separate from the (potentially) slower cache integration tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Dillon on 3/5/22.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+}
